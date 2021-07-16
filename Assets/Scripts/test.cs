cfee8fdab2301e2f669723f6af778c23cef88c2a
GeunTae KIM <k031900@gmail.com>
2021-07-16 12:04:00 +0900

    1

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //CapsuleCollider playerCol;
+
+    private void Start()
+    {
+        //playerCol.GetComponent<CapsuleCollider>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log(col.tag + "감지");
+        }
+        else if (col.tag == "Range")
+        {
+            Debug.Log(col.tag + "감지");
+        }
+    }
+}
