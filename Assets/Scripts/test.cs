62936f0d10fb12133f270549833b12df4893b32a
GeunTae KIM <k031900@gmail.com>
2021-07-18 12:52:48 +0900

    fun

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     //CapsuleCollider playerCol;
+    Vector3Int cellPosition;
+    public Grid grid;
 
     private void Start()
-    {
-        //playerCol.GetComponent<CapsuleCollider>();
+    {        
+        cellPosition = grid.WorldToCell(transform.position);
+        //transform.position = grid.GetCellCenterWorld(cellPosition);
     }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player")
         {
-            Debug.Log(col.tag + "감지");
+            //Debug.Log(col.tag + "감지");
+            Debug.Log("플레이어가 접근한 셀의 위치: " + cellPosition);
+
+        
         }
-        else if (col.tag == "Range")
-        {
-            Debug.Log(col.tag + "감지");
-        }
+        //else if (col.tag == "Range")
+        //{
+            //Debug.Log(col.tag + "감지");
+        //}
+
+        
     }
 }
